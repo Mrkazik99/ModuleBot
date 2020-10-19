8c3f03ea4c84e36a61727e967333655803af37f3
Mrkazik99 <mateuszkazmierczak99@gmail.com>
2020-10-19 19:41:42 +0200

    Added dices module

--- modules/dices.py
+++ modules/dices.py
@@ -4,11 +4,11 @@ import json
 
 async def roll_dice():
     my_dict = {
-        'Tetrahedron': 'Tetrahedron: ' + str(random.randint(1, 4)),
-        'Cube': 'Cube: ' + str(random.randint(1, 6)),
-        'Octahedron': 'Octahedron: ' + str(random.randint(1, 8)),
-        'Pentagonal trapezohedron': 'Pentagonal trapezohedron: ' + str(random.randint(1, 10)),
-        'Dodecahedron': 'Dodecahedron: ' + str(random.randint(1, 12)),
-        'Icosahedron': 'Icosahedron: ' + str(random.randint(1, 20))
+        'Tetrahedron': '{"desc": "Tetrahedron", "text": "Tetrahedron: ' + str(random.randint(1, 4))+'", "withImage": "False"}',
+        'Cube': '{"desc": "Cube", "text": "Cube: ' + str(random.randint(1, 6))+'", "withImage": "False"}',
+        'Octahedron': '{"desc": "Octahedron", "text": "Octahedron: ' + str(random.randint(1, 8))+'", "withImage": "False"}',
+        'Pentagonal trapezohedron': '{"desc": "Pentagonal trapezohedron", "text": "Pentagonal trapezohedron: ' + str(random.randint(1, 10))+'", "withImage": "False"}',
+        'Dodecahedron': '{"desc": "Dodecahedron", "text": "Dodecahedron: ' + str(random.randint(1, 12))+'", "withImage": "False"}',
+        'Icosahedron': '{"desc": "Icosahedron", "text": "Icosahedron: ' + str(random.randint(1, 20))+'", "withImage": "False"}'
     }
     return my_dict
--- moduleslist.py
+++ moduleslist.py
@@ -2,14 +2,14 @@ from modules import horoscopes
 from modules import dices
 
 functions_list = {
-    # 'Roll a dice': '{"text": "Usage: @ModuleTestKazikBot !dices", "desc": "!dices"}',
+    'Roll a dice': '{"text": "Usage: @ModuleTestKazikBot !dices", "desc": "!dices"}',
     'Daily horoscope': '{"text": "Usage: @ModuleTestKazikBot !horoscopes", "desc": "!horoscopes"}'
 }
 
 
 async def create_answer(module):
-    # if module == "!dices":
-    #     return await dices.roll_dice()
+    if module == "!dices":
+        return await dices.roll_dice()
     if module == "!horoscopes":
         return await horoscopes.get_horo()
     else:
